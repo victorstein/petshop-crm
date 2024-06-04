e0cc493cf5dc022e6f760b4c971ab342c9919fa2
victorstein <stein.hakase.vs@gmail.com>
2024-06-04 00:24:04 -0600

    ♻️	Refactored Firebase integration to use firebase-admin and fireorm

--- integrations/firebase/index.tsx
+++ integrations/firebase/index.tsx
@@ -1,15 +1,11 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import admin from 'firebase-admin'
+import * as fireorm from 'fireorm'
 
-// Initialize Firebase
-export const firebase = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+const serviceAccount = require('./firebase-key.json')
+
+admin.initializeApp({
+  credential: serviceAccount
 })
 
-export const db = getFirestore(firebase)
+const db = admin.firestore()
+fireorm.initialize(db, { validateModels: true })
