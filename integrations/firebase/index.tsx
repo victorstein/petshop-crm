222ace5fd377ccfcd6f245cc31001dd1c961e2f1
victorstein <stein.hakase.vs@gmail.com>
2024-06-19 20:28:58 -0600

    ♻️ reverted fireorm admin sdk requirements

--- integrations/firebase/index.tsx
+++ integrations/firebase/index.tsx
@@ -1,11 +1,15 @@
-import admin from 'firebase-admin'
-import * as fireorm from 'fireorm'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
-const serviceAccount = require('./firebase-key.json')
-
-admin.initializeApp({
-  credential: serviceAccount
+// Initialize Firebase
+export const firebase = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 })
 
-const db = admin.firestore()
-fireorm.initialize(db, { validateModels: true })
+export const db = getFirestore(firebase)
