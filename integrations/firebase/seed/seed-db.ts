1231180cc65cfbe2495bdc5d4cd36232075aaa8e
victorstein <stein.hakase.vs@gmail.com>
2024-06-24 23:07:55 -0600

    🌱 Modify seed script to make look up

--- integrations/firebase/seed/seed-db.ts
+++ integrations/firebase/seed/seed-db.ts
@@ -1,14 +1,22 @@
 import 'reflect-metadata'
+import 'dotenv/config'
 import { createDummyBreed } from 'tests/fixtures/breed'
 import { createDummyShelter } from 'tests/fixtures/shelter'
 import { createDummyShelteredAnimal } from 'tests/fixtures/sheltered-animal'
 import { createDummyUser } from 'tests/fixtures/user'
+import { shelteredAnimalRepository } from '../schema/sheltered-animal'
 
 const seedDb = async (): Promise<void> => {
   const shelter = await createDummyShelter()
   const breed = await createDummyBreed()
-  await createDummyShelteredAnimal(shelter.id, breed.id)
+  const shelteredAnimal = await createDummyShelteredAnimal(shelter.id, breed.id)
   await createDummyUser()
+
+  const foundShelteredAnimal = await shelteredAnimalRepository.findById(
+    shelteredAnimal.id.id,
+    { pupulate: true }
+  )
+  console.log('Sheltered Animal found:', foundShelteredAnimal)
 
   console.log('Database seeded successfully 🌱')
   process.exit(0)
