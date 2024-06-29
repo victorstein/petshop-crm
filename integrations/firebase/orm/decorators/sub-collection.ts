434646f3f40e96e8ead3c0739b30332f9e054768
victorstein <stein.hakase.vs@gmail.com>
2024-06-28 21:53:11 -0600

    ✨ Added key type to metadata collection

--- integrations/firebase/orm/decorators/sub-collection.ts
+++ integrations/firebase/orm/decorators/sub-collection.ts
@@ -1,14 +1,20 @@
 import type { Constructor } from '../types'
 
+export interface SubCollectionMetadata {
+  key: string
+  type: 'Array' | string
+}
+
 export const SubCollection = <T>(name?: string) => {
   return function (targetClass: T, propertyKey: string) {
     const target = targetClass as Constructor<T>
     const key = name ?? propertyKey
+    const type = Reflect.getMetadata('design:type', target, propertyKey).name
 
     const subCollections = Reflect.getMetadata('subCollections', target) ?? []
     Reflect.defineMetadata(
       'subCollections',
-      { ...subCollections, [propertyKey]: { key } },
+      { ...subCollections, [propertyKey]: { key, type } },
       target
     )
   }
